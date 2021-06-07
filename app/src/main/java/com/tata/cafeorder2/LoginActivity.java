ca5ed99f2a33621f131c9f5ee633f0a3e03cdf9f
Татяна Н <naumovets.ts@gmail.com>
2021-06-07 19:11:59 +0300

    Make the activity_login.xml

---
+++ app/src/main/java/com/tata/cafeorder2/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.tata.cafeorder2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
